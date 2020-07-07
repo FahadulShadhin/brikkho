3e0fa850b1904c765461fecb73659ccc5bcb5795
shadhin <fahadulshadhin17@gmail.com>
2020-07-07 18:35:05 +0600

    first commit

---
+++ brikkho/store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+    ###
+    path('plants/', views.plants, name='plants'),
+    path('seeds/', views.seeds, name='seeds'),
+    path('cuttings/', views.cuttings, name='cuttings'),
+    path('fruits/', views.fruits, name='fruits'),
+    path('flowers/', views.flowers, name='flowers'),
+    path('vegetables/', views.vegetables, name='vegetables'),
+]
