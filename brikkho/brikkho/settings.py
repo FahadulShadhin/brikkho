bfdc74b3953a30d65eba1c63c50eb773c5c59de0
Shadhin <fahadulshadhin17@gmail.com>
2021-06-11 09:44:48 +0600

    Update settings.py

--- brikkho/brikkho/settings.py
+++ brikkho/brikkho/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=mxc@w))1#=#)t*ml++bpd!%!iyptkxheo40r*5@l6eq2vlq3)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
